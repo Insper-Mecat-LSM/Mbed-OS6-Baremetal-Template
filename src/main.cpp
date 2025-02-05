c866f2997ba380f842901bacd5d7084e942c29fe
kaiqued1 <kaiqued1@insper.edu.br>
2025-02-05 10:14:25 -0300

    Upload-Files

---
+++ src/main.cpp
@@ -0,0 +1,20 @@
+/* mbed Microcontroller Library
+ * Copyright (c) 2019 ARM Limited
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#include "mbed.h"
+
+
+DigitalOut led(PA_5);
+
+int main()
+{
+    printf("This is the bare metal blinky example running on Mbed OS %d.%d.%d.\n", MBED_MAJOR_VERSION, MBED_MINOR_VERSION, MBED_PATCH_VERSION);
+
+    while (true)
+    {
+        led = !led;
+        wait_us(500000);
+    }
+}
